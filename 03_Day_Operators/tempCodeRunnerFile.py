4ce2cc5952b8829db04f9bc98da5e18564c49185
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-21 20:04:57 +0700

    day03_6

--- 03_Day_Operators/tempCodeRunnerFile.py
+++ 03_Day_Operators/tempCodeRunnerFile.py
@@ -1,2 +1,11 @@
-for i in range(1,6):
-    print(i, 1, i, i**2, i**3)
+import numpy as np
+def findY(x):
+    return x**2 + 6*x + 9
+    
+x_val = np.arange(-10,10,1)
+y_val = [findY(x) for x in x_val]
+
+roots = np.roots([1,6,9])
+print(f"Giá trị x: {x_val}")
+print(f"Giá trị y: {y_val}\n")
+print(roots)
