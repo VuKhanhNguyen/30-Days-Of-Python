8fc6302320a8468d2347d193a800cd211edf1a4f
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-24 10:43:03 +0700

    day05_1

---
+++ 05_Day_Lists/bai1.py
@@ -0,0 +1,15 @@
+# 1. Declare an empty list
+# 2. Declare a list with more than 5 items
+# 3. Find the length of your list
+# 4. Get the first item, the middle item and the last item of the list
+
+list_a = []
+list_b = ['Banana', 'Grape', 'Orange', 'Lemon','Watermelon', 'Strawberry', 'Potato']
+print(len(list_b))
+
+print(list_b[0])
+print(list_b[-1])
+
+index = int(len(list_b)/2)
+print(list_b[index])
+    
