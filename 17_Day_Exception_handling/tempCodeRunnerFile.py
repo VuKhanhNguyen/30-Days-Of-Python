1d61adb8b0a006c2207f7e29101b503335794bab
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-03-31 23:24:17 +0700

    day17_01

---
+++ 17_Day_Exception_handling/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+# nordic_countries = names[:5]
+# es = names[5]
+# ru = names[6]
+*nordic_countries, es, ru = names
+print(nordic_countries, es, ru)
